283ca0332afe978f262bc55623c6ed9eef83c8d6
Scot Matson <scot@area1security.com>
2016-07-28 13:48:12 -0700

    Added offline browsers

--- browsers/README.d
+++ browsers/README.d
@@ -5,3 +5,4 @@ A collection of browser user-agents
 ##Attributions
 * http://www.useragentstring.com/pages/useragentstring.php?typ=Browser
 * http://www.useragentstring.com/pages/useragentstring.php?typ=Mobile%20Browser
+* http://www.useragentstring.com/pages/useragentstring.php?typ=Offline%20Browser
