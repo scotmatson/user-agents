846018beb285959e0076ca3f6fe7bfccacf1c6af
Scot Matson <scot@area1security.com>
2016-07-28 13:43:17 -0700

    Added mobile browsers

--- browsers/README.d
+++ browsers/README.d
@@ -4,3 +4,4 @@ A collection of browser user-agents
 
 ##Attributions
 * http://www.useragentstring.com/pages/useragentstring.php?typ=Browser
+* http://www.useragentstring.com/pages/useragentstring.php?typ=Mobile%20Browser
